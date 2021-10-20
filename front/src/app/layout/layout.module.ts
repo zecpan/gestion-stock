01c21cab8271e9f42cc56b219695764c30f6f51a
Zecpan <loin.maxime@gmail.com>
2021-10-20 11:15:17 +0200

    dev: added 3 components in layout

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
   imports: [
     CommonModule
   ]
