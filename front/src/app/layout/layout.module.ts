9d8b7fe7de4e96fbf1c94448bf333be426f4ecaa
Zecpan <loin.maxime@gmail.com>
2021-10-20 12:12:48 +0200

    dev: header

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -14,6 +14,9 @@ import { BodyComponent } from './body/body.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HeaderComponent,FooterComponent,BodyComponent
   ]
 })
 export class LayoutModule { }
