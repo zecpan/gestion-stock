827f50e93c6afc2d3c0e1da283cc2ac4ad117ab7
Zecpan <loin.maxime@gmail.com>
2021-10-20 17:03:14 +0200

    dev: initialisation de la liste des articles

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class StockComponent {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 1.23, qty: 234 },
+    { name: 'Pelle', price: 2.4, qty: 120 },
+    { name: 'Pince', price: 3, qty: 5 },
+    { name: 'Marteau', price: 5, qty: 1200 },
+  ];
 }
