779e503b3a752e56f354c62d03a73721991b123b
Zecpan <loin.maxime@gmail.com>
2021-10-21 12:09:21 +0200

    dev: implemented directive with select

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
-export class AutofocusDirective {
-  constructor() {
+export class AutofocusDirective implements AfterViewInit {
+  constructor(private elt: ElementRef<HTMLInputElement>) {
     console.log('instatiate autoFocus directive');
+  }
+  ngAfterViewInit(): void {
+    this.elt.nativeElement.select();
   }
 }
