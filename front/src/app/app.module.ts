cb4647bdbfc0c311ed05644fae2f677b5932d31c
Zecpan <loin.maxime@gmail.com>
2021-10-21 14:01:53 +0200

    dev: added httpClient module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import localeFr from '@angular/common/locales/fr';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,15 +8,18 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
+import { ArticleService } from './services/article.service';
+import { HttpArticleService } from './services/http-article.service';
 
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
   providers: [
     { provide: LOCALE_ID, useValue: 'fr' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: ArticleService, useClass: HttpArticleService },
   ],
   bootstrap: [AppComponent],
 })
---
+++ front/src/app/services/http-article.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ArticleService } from './article.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpArticleService extends ArticleService {
+  constructor(private http: HttpClient) {
+    super();
+    console.log('http-article service');
+  }
+}
