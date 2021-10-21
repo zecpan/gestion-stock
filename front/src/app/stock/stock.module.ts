ba520d5657c5f3695adf31b23ae7d369ab6c6037
Zecpan <loin.maxime@gmail.com>
2021-10-21 11:55:27 +0200

    dev: added appAutofocus Directive

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { WidgetModule } from '../widget/widget.module';
 import { AddComponent } from './add/add.component';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 
 @NgModule({
   declarations: [StockComponent, AddComponent],
-  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    StockRoutingModule,
+    ReactiveFormsModule,
+    WidgetModule,
+  ],
 })
 export class StockModule {}
