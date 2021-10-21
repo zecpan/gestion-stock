8b37b7926769674e757c8d96c8f00ceff260d320
Zecpan <loin.maxime@gmail.com>
2021-10-21 15:33:06 +0200

    dev: cors problem

--- back/server.ts
+++ back/server.ts
@@ -1,11 +1,15 @@
 console.log("About to start the server...");
 import express from "express";
 import serveIndex from "serve-index";
-import morgan from 'morgan';
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 const dir = ".";
+
+//cors
+app.use(cors());
 
 app.use(morgan("tiny"));
 
