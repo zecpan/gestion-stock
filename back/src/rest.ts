a2294926014508f72520bcb8034407683dcd33eb
Zecpan <loin.maxime@gmail.com>
2021-10-22 16:16:07 +0200

    lint: added lint

--- back/src/rest.ts
+++ back/src/rest.ts
@@ -3,9 +3,6 @@ import { Resource } from "./interfaces/Resource";
 import { ResourceServiceFactory } from "./services/ResourceServiceFactory";
 
 export function rest<T extends Resource>(resourceName: string) {
-  function generateId() {
-    return Date.now() + "_" + Math.floor(Math.random() * 1e6);
-  }
   const app = express.Router();
   const resourceService = ResourceServiceFactory.get(
     process.env.GESTION_STOCK_DBTYPE || "ram",
